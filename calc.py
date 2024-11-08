bbca9fe6f049463c891ff09ff7c36625f2424f7e
mumtazalee <125261688+mumtazalee@users.noreply.github.com>
2024-11-08 11:37:56 +0000

    Create calc.py
    Assessment 2

---
+++ calc.py
@@ -0,0 +1,12 @@
+num1 = int(input("Give a number"))
+num2 = int(input("Give a number"))
+
+sum = num1+num2
+diff = num1-num2
+div = num1/num2
+prod = num1*num2
+
+print("the sum is ", sum)
+print("the diff is ", diff)
+print("the division is ", div)
+print("the sum product ", prod)
