8a3f6d76737054437abc847eae86098eeebe5fa5
mumtazalee <125261688+mumtazalee@users.noreply.github.com>
2024-11-29 13:34:13 +0000

    Create module.py

---
+++ module.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+
+
+print(f'The sum of {num1} and {num2} is {sum}')
+print(f'The difference between {num1} and {num2} is {difference}')
+print(f'The product of {num1} and {num2} is {product}')
+print(f'The quotient of {num1} and {num2} is {quotient}')
+
